1adda1883971e93996cde30954498c6b68bec19b
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2022-02-04 04:06:18 +0300

    Tower swap logic

---
+++ Assets/Scripts/UI/TowerButtons/TowerSwapContent.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TowerSwapContent : MonoBehaviour
+{
+    [SerializeField, Range(1, 100)] private int percentageOfRemainingTiles;
+    [Space]
+    [SerializeField] private TMP_Text tilesToSwapText;
+    [SerializeField] private List<TowerSwapButton> swapButtons = new List<TowerSwapButton>();
+
+    private int _tilesToSwap;
+    private TowerBuildPlatform _tower;
+    private TileSetter _playerTileSetter;
+    private UserData _data;
+
+    public int TilesToSwap
+    {
+        get
+        {
+            if (_tower)
+            {
+                int t = Mathf.RoundToInt((_tower.TilesToUpgrade * percentageOfRemainingTiles) / 100);
+                return _tilesToSwap = Mathf.Clamp(t, 0, _data.MaxTiles);
+            }
+
+            Debug.LogWarning("Tower is null, cant set tiles to swap");
+            return 0;
+        }
+       
+    }
+
+    public void Init(TowerBuildPlatform tower, TileSetter playerTiles)
+    {
+        _tower = tower;
+
+        if(_playerTileSetter == null)
+            _playerTileSetter = playerTiles;
+
+        if (_data == null)
+            _data = GameController.Data;
+        
+        InitSwapButtons(tower);
+        tilesToSwapText.SetText(TilesToSwap.ToString());
+    }
+
+    private void InitSwapButtons(TowerBuildPlatform tower)
+    {
+        for (int i = 0; i < swapButtons.Count; i++)
+        {
+            swapButtons[i].Init(tower, this, _playerTileSetter);
+
+            if (swapButtons[i].Type == tower.ActiveTower.Data.Type)
+                swapButtons[i].gameObject.SetActive(false);
+            else
+                swapButtons[i].gameObject.SetActive(true);
+
+        }
+    }
+
+   
+   
+
+}
